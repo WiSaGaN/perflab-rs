01553c16d6acd685b7e67b0eef75c0d9d80afd55
Wangshan Lu <wisagan@gmail.com>
2016-07-01 17:39:34 +0800

    Reformat code

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,17 @@
 #![feature(test)]
 extern crate test;
 extern crate time;
-pub use time::{at, get_time, Timespec};
-pub use std::sync::mpsc::{sync_channel, Sender};
 
 #[cfg(test)]
 mod g1_generic {
-    use std;
-    use super::*;
     use test::Bencher;
+    use std::thread;
+    use std::time::Duration;
+    use time::get_time;
 
     #[bench]
     fn t01_one_plus_one(b: &mut Bencher) {
-        b.iter(|| 1 + 1 );
+        b.iter(|| 1 + 1);
     }
 
     #[bench]
@@ -22,67 +21,76 @@ mod g1_generic {
 
     #[bench]
     fn t03_with_one_ms_setup(b: &mut Bencher) {
-        std::thread::sleep(std::time::Duration::from_millis(1));
-        b.iter(|| 1 + 1 );
+        thread::sleep(Duration::from_millis(1));
+        b.iter(|| 1 + 1);
     }
 }
 
 #[cfg(test)]
 mod g2_sync_channel {
-    use super::*;
     use test::Bencher;
+    use std::sync::mpsc::sync_channel;
 
     #[bench]
     fn t01_send_unit(b: &mut Bencher) {
         let (s, _) = sync_channel(1024000);
-        b.iter(|| { s.send(()) });
+        b.iter(|| s.send(()));
     }
 
     #[bench]
     fn t02_send_integer(b: &mut Bencher) {
         let (s, _) = sync_channel(1024000);
-        b.iter(|| { s.send(42) });
+        b.iter(|| s.send(42));
     }
 
     #[bench]
     fn t03_send_string(b: &mut Bencher) {
         let (s, _) = sync_channel(1024000);
-        b.iter(|| { s.send(String::from("The quick brown fox jumps over the lazy dog.")) });
+        b.iter(|| s.send(String::from("The quick brown fox jumps over the lazy dog.")));
     }
 
     #[bench]
     fn t04_send_recv_unit(b: &mut Bencher) {
         let (s, r) = sync_channel(1024000);
-        b.iter(|| { s.send(()).unwrap(); r.recv() });
+        b.iter(|| {
+            s.send(()).unwrap();
+            r.recv()
+        });
     }
 
     #[bench]
     fn t05_send_recv_integer(b: &mut Bencher) {
         let (s, r) = sync_channel(1024000);
-        b.iter(|| { s.send(42).unwrap(); r.recv() });
+        b.iter(|| {
+            s.send(42).unwrap();
+            r.recv()
+        });
     }
 
     #[bench]
     fn t06_send_recv_string(b: &mut Bencher) {
         let (s, r) = sync_channel(1024000);
-        b.iter(|| { s.send(String::from("The quick brown fox jumps over the lazy dog.")).unwrap(); r.recv() });
+        b.iter(|| {
+            s.send(String::from("The quick brown fox jumps over the lazy dog.")).unwrap();
+            r.recv()
+        });
     }
 }
 
 #[cfg(test)]
 mod g3_formatting {
-    use super::*;
     use test::Bencher;
     use std::thread;
+    use time::{at, get_time};
 
     #[bench]
     fn t01_format_simple_string(b: &mut Bencher) {
-        b.iter(|| { format!("hello, world") });
+        b.iter(|| format!("hello, world"));
     }
 
     #[bench]
     fn t02_format_debug_get_time(b: &mut Bencher) {
-        b.iter(|| { format!("now: {:?}", get_time()) });
+        b.iter(|| format!("now: {:?}", get_time()));
     }
 
     #[bench]
@@ -100,7 +108,7 @@ mod g3_formatting {
                     tm.tm_sec,
                     tm_millisec,
                     tm_microsec,
-                    tm.tm_utcoff/3600,
+                    tm.tm_utcoff / 3600,
                     thread::current().name().unwrap_or_default(),
                     module_path!(),
                     file!(),
@@ -110,4 +118,3 @@ mod g3_formatting {
         });
     }
 }
-
